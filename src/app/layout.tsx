c35504b4c775f16157ab540f7c6b62b96e6b6ce2
ryderhorne <ryderhornedev@gmail.com>
2024-05-24 14:34:09 -0700

    Font optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "~/styles/globals.css";
-import { GeistSansNonVariable } from "geist/font/sans-non-variable";
-import { GeistMonoNonVariable } from "geist/font/mono-non-variable";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import { TRPCReactProvider } from "~/trpc/react";
 import { cn } from "~/lib/utils";
 import { Header, Footer } from "~/components/global";
@@ -38,7 +38,7 @@ export default function RootLayout({
 
   return (
     <html className="scroll-smooth bg-slate-50 selection:bg-slate-200/60 subpixel-antialiased" lang={locale}>
-      <body className={cn("flex flex-col min-h-screen text-lg text-slate-900 fill-slate-900", GeistSansNonVariable.className, GeistMonoNonVariable.variable)}>
+      <body className={cn("flex flex-col min-h-screen text-lg text-slate-900 fill-slate-900", GeistSans.className, GeistMono.variable)}>
         <NextIntlClientProvider locale={locale} messages={pick(messages, "name", "menu", "home", "portfolio.name", "services.name", "services.contact", "services.design", "services.development", "services.shopify", "services.seo", "contact.name", "contact.description", "contact.form.title", "contact.email", "contact.call", "contact.instagram", "contact.x", "contact.facebook", "contact.tiktok", "sitemap.name", "close")}>
           <Header />
         </NextIntlClientProvider>
