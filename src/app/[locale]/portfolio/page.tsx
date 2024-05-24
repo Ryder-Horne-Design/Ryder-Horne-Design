5ff70d49f46eb406c0d29bf2fc91ed618cfe7882
ryderhorne <ryderhornedev@gmail.com>
2024-05-24 15:15:38 -0700

    Optimized portfolio carousel items for smaller devices

--- src/app/[locale]/portfolio/page.tsx
+++ src/app/[locale]/portfolio/page.tsx
@@ -42,9 +42,9 @@ export default function Portfolio() {
           <CarouselContent>
             {
               websites.map(({ namespace, src }, index) => (
-                <CarouselItem className="basis-1/3" key={index + 1}>
+                <CarouselItem className="basis-full sm:basis-1/2 lg:basis-1/3" key={index + 1}>
                   <Link href={"/portfolio/" + (index + 1).toString()} className="flex flex-col">
-                    <Image src={src} alt={t("websites." + namespace + ".name")} width={2880} height={2048} sizes="40vw" className="rounded-xl mb-2 border-[6px] border-transparent bg-[linear-gradient(to_bottom_right,#0EA5E9,#34D399)] bg-[length:calc(100%+12px)_calc(100%+12px)] bg-[position:-6px_-6px] bg-no-repeat object-cover shadow lg:basis-1/2" />
+                    <Image src={src} alt={t("websites." + namespace + ".name")} width={2880} height={2048} sizes="(min-width: 1024px) 20vw, (min-width: 768px) 40vw, 80vw" className="rounded-xl mb-2 border-[6px] border-transparent bg-[linear-gradient(to_bottom_right,#0EA5E9,#34D399)] bg-[length:calc(100%+12px)_calc(100%+12px)] bg-[position:-6px_-6px] bg-no-repeat object-cover shadow lg:basis-1/2" />
                     <p className="text-base font-semibold">{t("websites." + namespace + ".name")}</p>
                     <p className="text-sm text-ellipsis line-clamp-1 text-slate-700/80">{t("websites." + namespace + ".description")}</p>
                   </Link>
